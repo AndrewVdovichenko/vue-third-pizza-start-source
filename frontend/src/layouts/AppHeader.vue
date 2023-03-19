25d9a9e75a2d6b983488a91277a79aaa039d3b53
Andrew Vdovichenko <andrewvdovichenko@MacMiniM2.local>
2023-03-19 10:26:00 +0500

    1.17.1 - add AppHeader.vue

---
+++ frontend/src/layouts/AppHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+
+    <!-- Logo -->
+    <div class="header__logo">
+      <a href="index.html" class="logo">
+        <img src="img/logo.svg" alt="V!U!E! Pizza logo" width="90" height="40">
+      </a>
+    </div>
+
+    <!-- Cart -->
+    <div class="header__cart">
+      <a href="cart.html">
+        0 ₽
+      </a>
+    </div>
+
+    <!-- User -->
+    <div class="header__user">
+      <a href="#" class="header__login">
+        <span>
+          Войти
+        </span>
+      </a>
+    </div>
+
+  </header>
+</template>
